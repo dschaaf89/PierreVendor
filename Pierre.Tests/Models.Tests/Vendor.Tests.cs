812a1db60eb87e514620cc762b07173d4f7b439d
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-02 09:13:43 -0700

    it will pass the getAll method test

--- Pierre.Tests/Models.Tests/Vendor.Tests.cs
+++ Pierre.Tests/Models.Tests/Vendor.Tests.cs
@@ -1,44 +1,66 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Pierre.Models;
-namespace Pierre.Tests
-{
+namespace Pierre.Tests {
   [TestClass]
-  public class VendorTest
-  {
-    [TestMethod]
-    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+  public class VendorTest :IDisposable {
+     public void Dispose()
     {
-      Vendor newVendor = new Vendor("test","test");
-      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor () {
+      Vendor newVendor = new Vendor ("test", "test");
+      Assert.AreEqual (typeof (Vendor), newVendor.GetType ());
     }
 
     [TestMethod]
-    public void GetName_ReturnsName_String()
-  {
-    //Arrange
-    string name = "Test name";
-    Vendor newVendor = new Vendor(name,"test");
+    public void GetName_ReturnsName_String () {
+      //Arrange
+      string name = "Test name";
+      Vendor newVendor = new Vendor (name, "test");
 
-    //Act
-    string result = newVendor.Name;
+      //Act
+      string result = newVendor.Name;
 
-    //Assert
-    Assert.AreEqual(name, result);
-  }
-   [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-  {
-    //Arrange
-    string description = "Test description";
-    Vendor newVendor = new Vendor("name",description);
+      //Assert
+      Assert.AreEqual (name, result);
+    }
 
-    //Act
-    string result = newVendor.Description;
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String () {
+      //Arrange
+      string description = "Test description";
+      Vendor newVendor = new Vendor ("name", description);
 
-    //Assert
-    Assert.AreEqual(description, result);
-  }
+      //Act
+      string result = newVendor.Description;
+
+      //Assert
+      Assert.AreEqual (description, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllCategoryObjects_CategoryList () 
+    {
+      //Arrange
+      string name01 = "bread";
+      string name02 = "Cookie";
+      Vendor newVendor1 = new Vendor (name01,"bread store");
+      Vendor newVendor2 = new Vendor (name02,"cookie store");
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+      List<Vendor> newResult = Vendor.GetAll();
+      //Act
+      foreach (Vendor item in newResult)
+      {
+        Console.WriteLine(item.Name);
+      }
+     
+
+      //Assert
+      CollectionAssert.AreEqual (newList, newResult);
+    }
 
   }
 }
