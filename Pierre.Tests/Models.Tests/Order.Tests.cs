f061220b41576efd6b02e8d341b669b9b8ed31f8
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-10-02 09:58:51 -0700

    added the remaining tests for the order class and wrote the methoids

--- Pierre.Tests/Models.Tests/Order.Tests.cs
+++ Pierre.Tests/Models.Tests/Order.Tests.cs
@@ -6,11 +6,11 @@ using Pierre.Models;
 namespace Pierre.Tests
 {
     [TestClass]
-  public class OrderTest {
+  public class OrderTest : IDisposable {
 
-    // public void Dispose () {
-    //   Item.ClearAll ();
-    // }
+    public void Dispose () {
+      Order.ClearAll ();
+    }
 
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order() 
@@ -30,6 +30,77 @@ namespace Pierre.Tests
 
       //Assert
       Assert.AreEqual (title, result);
+    }
+    [TestMethod]
+    public void SetDescription_SetDescription_String () {
+      //Arrange
+      string description = "croissants.";
+      Order newOrder = new Order ("bobs Store",description,30);
+
+      //Act
+      string updatedDescription = "sourdough bread";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual (updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList () {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll ();
+
+      // Assert
+      CollectionAssert.AreEqual (newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList () {
+      //Arrange
+      string description01 = "sourdough bread and cookies";
+      string description02 = "cookies and cakes";
+      Order newOrder1 = new Order ("bob",description01,5);
+      Order newOrder2 = new Order ("green",description02,10);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll ();
+
+      //Assert
+      CollectionAssert.AreEqual (newList, result);
+
+    }
+
+    [TestMethod]
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int () {
+      //Arrange
+     string description = "sourdough bread and cookies.";
+    Order newOrder = new Order("bob",description,15);
+
+      //Act
+      int result = newOrder.Id;
+
+      //Assert
+      Assert.AreEqual (1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order () {
+      //Arrange
+      string description01 = "sourdough bread and cookies";
+      string description02 = "cookies and cakes";
+      Order newOrder1 = new Order ("bob",description01,20);
+      Order newOrder2 = new Order ("fred",description02,15);
+
+      //Act
+        Order result = Order.Find(2);
+
+      //Assert
+      Assert.AreEqual (newOrder2, result);
     }
 
 
